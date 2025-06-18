23ec4ac8c71cbbbf1f33789b27410a843bb620a2
vitalspace <myglobalsounds@gmail.com>
2025-06-18 13:55:03 -0600

    Fix global is not defined error

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,14 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  define: {
+    global: 'window',
+    'process.env.NODE_ENV': '"development"'
+  },
+  resolve: {
+    alias: {
+      buffer: 'buffer',
+      process: 'process/browser'
+    }
+  }
 })
